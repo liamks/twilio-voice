2bbc7a04dc0a560306a0d6227e45965487b9ebc7
Liam Kaufman <liam.kaufman@gmail.com>
2015-07-15 16:12:42 -0400

    started survey

---
+++ libs/survey.js
@@ -0,0 +1,36 @@
+/*
+
+getNextQuestion(sid)
+  1. check if sid exists
+    a. if no, 
+      GET user info, store each step in redis
+      when complete getFirstQuestion
+    b. if yes, getNextQuestion
+  2. is Question null
+    a. if yes, survey is over (return null)
+    b. if no, textToSpeech.create (return url)
+  
+*/
+
+function Survey(){
+  Survey.redis = redis.createClient();
+  return Survey;
+}
+
+
+Survey._hasSurveyStarted = function _hasSurveyStarted(){
+
+};
+
+Survey._getQuestion = function _getQuestion(){
+
+};
+
+Survey._textToSpeech = function _textToSpeech(){
+
+};
+
+Survey.getNextQuestion = function getNextQuestion(sid){
+  var obj
+  return 
+};
